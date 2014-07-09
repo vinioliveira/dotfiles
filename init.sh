5860ff7af12ee4176704d7749041b01c6655ca23
Vínicius Oliveira <oliveira.mvs@gmail.com>
2014-07-09 20:03:13 -0300

    Removing old init

--- init.sh
+++
@@ -1,8 +0,0 @@
-source $HOME/.bash/scripts/alias.sh
-source $HOME/.bash/scripts/export.sh
-source $HOME/.bash/scripts/macosx.sh
-source $HOME/.bash/scripts/other.sh
-source $HOME/.bash/scripts/options.sh
-source $HOME/.bash/scripts/utils.sh
-
-

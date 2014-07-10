cd4082b751a04ca2c0fe2691df39da8f5d50a2d8
Vínicius Oliveira <oliveira.mvs@gmail.com>
2014-07-10 08:38:47 -0300

    Adding new stuffs

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -15,8 +15,17 @@ alias xmldelete="curl -X DELETE -H 'Accept: application/xml'"
 alias xmlget="curl -X GET -H 'Accept: application/xml'"
 alias xmlpost="curl -X POST -H 'Accept: application/xml'"
 alias xmlput="curl -X PUT -H 'Accept: application/xml'"
+
 # Directory navigation aliases
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 alias .....='cd ../../../..'
+
+#Just give a try to Git alias thanks to Celso Dantas
+alias glog="git log --graph --pretty='format:%C(yellow)%h%C(cyan)%d%Creset %s => %C(green)%an%C(white), %C(red)%ar%Creset'"
+alias gca="git add --all && git commit -a"
+alias gl="git log --graph --pretty='format:%C(yellow)%h%C(cyan)%d%Creset %s => %C(green)%an%C(white), %C(red)%ar%Creset'"
+alias gs="git status"
+alias g='git'
+alias gd="git diff"

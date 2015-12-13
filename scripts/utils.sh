e5598ea603dfc916bd82e56f2694d9045e45f50e
Vínicius Oliveira <vinicius@Viniciuss-MBP.Home>
2015-12-13 10:01:46 -0300

    Removing cd utils script

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -3,12 +3,6 @@ reload() {
   source ~/.zshrc;
 }
 
-# list directory after cd; also save the last directory
-# and open it when a new tab is created.
-cd() {
-  builtin cd "${@:-$HOME}" && ls && pwd > $CDHISTORY;
-}
-
 # Check if given url is giving gzipped content
 #
 #   $ gzipped http://simplesideias.com.br

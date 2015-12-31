4ee7fd5e7a6ac97a30375f7820069364bb1e794c
Vinícius Oliveira <me@vinioliveira.com>
2015-12-31 02:09:59 -0200

    Updating dir name of dotfiles

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 if [ ! -d "$HOME/.dotfile" ]; then
   echo "Installing dotfile"
-  git clone --depth=1 https://github.com/vinioliveira/dotfiles.git "$HOME/.dotfile"
-  cd "$HOME/.dotfile"
+  git clone --depth=1 https://github.com/vinioliveira/dotfiles.git "$HOME/.dotfiles"
+  cd "$HOME/.dotfiles"
   # [ "$1" = "ask" ] && export ASK="true"
   rake install
 else

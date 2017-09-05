282e9cde112ba518f9257cbc443551dbf0ee6d45
Vinícius Oliveira <me@vinioliveira.com>
2017-09-05 20:32:07 -0300

    use standard fzf for search branch

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -92,6 +92,6 @@ fbr() {
   # branches=$(git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
   branches=$(git branch --all | grep -v HEAD) &&
   branch=$(echo "$branches" |
-    fzf-tmux --height 20% --reverse -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
+    fzf --height 20% --reverse -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
     git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
 }

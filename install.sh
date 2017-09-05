2cec9ccab0b0021896f174b586d7183c9089c06f
Vinícius Oliveira <me@vinioliveira.com>
2017-09-05 11:37:28 -0300

    using ssh url

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 if [ ! -d "$HOME/.dotfile" ]; then
   echo "Installing dotfile"
-  git clone --depth=1 https://github.com/vinioliveira/dotfiles.git "$HOME/.dotfiles"
+  git clone --depth=1 git@github.com:vinioliveira/dotfiles.git "$HOME/.dotfiles"
   cd "$HOME/.dotfiles"
   # [ "$1" = "ask" ] && export ASK="true"
   rake install

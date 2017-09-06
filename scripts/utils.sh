b29226faafd77decba906252d2ceaaee3c515d18
Vinícius Oliveira <me@vinioliveira.com>
2017-09-05 21:40:40 -0300

    colortab utils

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -95,3 +95,12 @@ fbr() {
     fzf --height 20% --reverse -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
     git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
 }
+
+colortab() {
+  for i in {0..255} ; do
+    printf "\x1b[48;5;%sm%3d\e[0m " "$i" "$i"
+    if (( i == 15 )) || (( i > 15 )) && (( (i-15) % 6 == 0 )); then
+      printf "\n";
+    fi
+  done
+}

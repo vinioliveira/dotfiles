f57b23943b91a54ebd0f873e4da8032d685600ef
Vinícius Oliveira <me@vinioliveira.com>
2018-01-23 10:08:50 -0300

    added copy and past capability to zsh vi mode

--- zsh/vi-mode.zsh
+++ zsh/vi-mode.zsh
@@ -5,3 +5,25 @@ export USE_EDITOR=$EDITOR
 export VISUAL=$EDITOR
 export KEYTIMEOUT=1
 
+# http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html
+pb-copy-region-as-kill-deactivate-mark () {
+  zle copy-region-as-kill
+  zle set-mark-command -n -1
+  echo -n $CUTBUFFER | pbcopy
+}
+zle -N pb-copy-region-as-kill-deactivate-mark
+bindkey -a "y" pb-copy-region-as-kill-deactivate-mark
+
+pb-kill-whole-line () {
+  zle kill-whole-line
+  echo -n $CUTBUFFER | pbcopy
+}
+zle -N pb-kill-whole-line
+bindkey -a "dd" pb-kill-whole-line
+
+pb-yank () {
+  CUTBUFFER=$(pbpaste)
+  zle yank
+}
+zle -N pb-yank
+bindkey -a  "p" pb-yank

a23876c5f0e05f41d7faf7c2e3ab9c1da65593df
Vinícius Oliveira <me@vinioliveira.com>
2018-04-10 08:19:03 -0300

    added fzf to work in terminal with same shortcut

--- zsh/key-bindings.zsh
+++ zsh/key-bindings.zsh
@@ -1,2 +1,14 @@
-bindkey '^r' history-incremental-search-backward
+# bindkey '^r' history-incremental-search-backward
+fzf-open-file-or-dir() {
+  local out=$(fzf --height 50% --reverse --exit-0 --preview 'cat {}')
+
+  if [ -f "$out" ]; then
+    $EDITOR "$out" < /dev/tty
+  elif [ -d "$out" ]; then
+    cd "$out"
+    zle reset-prompt
+  fi
+}
+zle     -N   fzf-open-file-or-dir
+bindkey '^P' fzf-open-file-or-dir
 

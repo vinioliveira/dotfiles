dcc334dfe770a2746674a94bc33966547e570103
Vinícius Oliveira <me@vinioliveira.com>
2019-01-28 09:35:32 -0300

    remove tmuxp completion

--- zsh/third-party.zsh
+++ zsh/third-party.zsh
@@ -2,17 +2,15 @@
 eval "$(rbenv init -)"
 
 #nvm start
+
 export NVM_DIR="/usr/local/var/nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
 # Init the fasd
 eval "$(fasd --init auto hub alias -s)"
 
 
 autoload bashcompinit
 bashcompinit
-
-eval "$(_TMUXP_COMPLETE=source tmuxp)"
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh

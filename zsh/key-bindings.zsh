fa521f4a3d49dfaae22956ceb7163c46a2e5d83a
Vinícius Oliveira <me@vinioliveira.com>
2019-02-17 11:59:34 -0300

    Create function without shortcut

--- zsh/key-bindings.zsh
+++ zsh/key-bindings.zsh
@@ -1,6 +1,5 @@
-# bindkey '^r' history-incremental-search-backward
-fzf-open-file-or-dir() {
-  local out=$(fzf --height 50% --reverse --exit-0 --preview 'cat {}')
+vfzf() {
+  local out=$(fzf-tmux --height 50% --reverse --exit-0 --preview 'cat {}')
 
   if [ -f "$out" ]; then
     $EDITOR "$out" < /dev/tty
@@ -9,6 +8,4 @@ fzf-open-file-or-dir() {
     zle reset-prompt
   fi
 }
-zle     -N   fzf-open-file-or-dir
-bindkey '^P' fzf-open-file-or-dir
 

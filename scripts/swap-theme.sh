d69b743fe426e2e3f50caf9d3ebd912dd8973e6e
Vinícius Oliveira <marcus@copy.ai>
2024-05-12 18:17:31 -0300

    alcirtty themes

--- scripts/swap-theme.sh
+++ scripts/swap-theme.sh
@@ -11,7 +11,7 @@ fi
 
 
 if [[ $ARG = "toggle" ]]; then
-  if cmp -s $ALACRITTY_CONFIG_BASEPATH/alacritty.yml $ALACRITTY_CONFIG_BASEPATH/alacritty.light.yml; then
+  if cmp -s $ALACRITTY_CONFIG_BASEPATH/alacritty.toml $ALACRITTY_CONFIG_BASEPATH/alacritty.light.toml; then
     ARG="dark"
   else
     ARG="light"
@@ -24,18 +24,18 @@ if [[ -L $NVIM_CONFIG_BASEPATH/init.lua ]]; then
   rm $NVIM_CONFIG_BASEPATH/init.lua
 fi
 
-if [[ -f $ALACRITTY_CONFIG_BASEPATH/alacritty.yml ]]; then
-  rm $ALACRITTY_CONFIG_BASEPATH/alacritty.yml
+if [[ -f $ALACRITTY_CONFIG_BASEPATH/alacritty.toml ]]; then
+  rm $ALACRITTY_CONFIG_BASEPATH/alacritty.toml
 fi
 
 if [[ $ARG = "light" ]]; then
-  # ln -s $ALACRITTY_CONFIG_BASEPATH/alacritty.light.yml $ALACRITTY_CONFIG_BASEPATH/alacritty.yml
+  # ln -s $ALACRITTY_CONFIG_BASEPATH/alacritty.light.toml $ALACRITTY_CONFIG_BASEPATH/alacritty.toml
   ln -s $NVIM_CONFIG_BASEPATH/init.light.lua $NVIM_CONFIG_BASEPATH/init.lua
-  cp $ALACRITTY_CONFIG_BASEPATH/alacritty.light.yml $ALACRITTY_CONFIG_BASEPATH/alacritty.yml
+  cp $ALACRITTY_CONFIG_BASEPATH/alacritty.light.toml $ALACRITTY_CONFIG_BASEPATH/alacritty.toml
 elif [[ $ARG = "dark" ]]; then
-  # ln -s $ALACRITTY_CONFIG_BASEPATH/alacritty.dark.yml $ALACRITTY_CONFIG_BASEPATH/alacritty.yml
+  # ln -s $ALACRITTY_CONFIG_BASEPATH/alacritty.dark.toml $ALACRITTY_CONFIG_BASEPATH/alacritty.toml
   ln -s $NVIM_CONFIG_BASEPATH/init.dark.lua $NVIM_CONFIG_BASEPATH/init.lua
-  cp $ALACRITTY_CONFIG_BASEPATH/alacritty.dark.yml $ALACRITTY_CONFIG_BASEPATH/alacritty.yml
+  cp $ALACRITTY_CONFIG_BASEPATH/alacritty.dark.toml $ALACRITTY_CONFIG_BASEPATH/alacritty.toml
 fi
 
 

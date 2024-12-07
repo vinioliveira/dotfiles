4b9ac421993f7b0f7d9c5245d8786444a99b34aa
Vinícius Oliveira <marcus@copy.ai>
2024-12-07 10:40:42 -0300

    two new nvim Plugins avante and refactoring

---
+++ nvim/lua/lazy/plugins/refactoring.lua
@@ -0,0 +1,22 @@
+return {
+  "ThePrimeagen/refactoring.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  lazy = false,
+  config = function()
+    require("refactoring").setup()
+
+    vim.keymap.set("x", "<space>re", function() require('refactoring').refactor('Extract Function') end)
+    vim.keymap.set("x", "<space>rf", function() require('refactoring').refactor('Extract Function To File') end)
+    -- Extract function supports only visual mode
+    --
+    vim.keymap.set("x", "<space>rv", function() require('refactoring').refactor('Extract Variable') end)
+    -- Inline var supports both normal and visual mode
+
+    vim.keymap.set("n", "<space>rb", function() require('refactoring').refactor('Extract Block') end)
+    vim.keymap.set("n", "<space>rbf", function() require('refactoring').refactor('Extract Block To File') end)
+    -- Extract block supports only normal mode
+  end,
+}

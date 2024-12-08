b2b40920dd9191e5e9175829e7769d0b8c7b73d6
Vinícius Oliveira <marcus@copy.ai>
2024-12-08 15:26:47 -0300

    Test

--- nvim/lua/lazy/plugins/futigive.lua
+++ nvim/lua/lazy/plugins/futigive.lua
@@ -4,11 +4,12 @@ return {
     local opts = { noremap = true, silent = true }
     vim.keymap.set("n", "gs", ":abo Git<CR>", opts)
     vim.keymap.set("n", "go", ":GBrowse<CR>", opts)
+    vim.keymap.set("v", "go", ":GBrowse<CR>", opts)
     vim.keymap.set("n", "gd", ":Gvdiff<CR>", opts)
     vim.keymap.set("n", "gu", ":Dispatch git up<CR>", opts)
     vim.keymap.set("n", "gp", ":echom('Pushing... ') | Git push<CR>", opts)
     vim.keymap.set("n", "<leader>gf", ":echom('Force pushing... ') | Git push --force-with-lease<CR>", opts)
     vim.keymap.set("n", "gb", ":Git blame<CR>", opts)
-    vim.keymap.set("v", "go", ":GBrowse<CR>", opts)
+    vim.keymap.set("n", "gpr", ":Dispatch gh pr create -f<CR>", opts)
   end
 }

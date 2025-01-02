bccad737ea038500ecd72166dfb5de6ce2111aa8
Vinícius Oliveira <marcus@copy.ai>
2025-01-02 07:54:16 -0300

    Update golang specifics

--- nvim/lua/lazy/plugins/nvim-lint.lua
+++ nvim/lua/lazy/plugins/nvim-lint.lua
@@ -7,6 +7,7 @@ return {
       typescriptreact = { 'eslint_d', 'cspell' },
       markdown = { "cspell" },
       lua = { "cspell" },
+      go = { "cspell" },
     }
 
     local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })

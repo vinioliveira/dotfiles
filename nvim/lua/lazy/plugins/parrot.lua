547737d29f0f3a45abeefbb062a90896f654c254
Vinícius Oliveira <marcus@copy.ai>
2025-01-25 15:33:09 -0300

    Update the new Parrot setup paremeters

--- nvim/lua/lazy/plugins/parrot.lua
+++ nvim/lua/lazy/plugins/parrot.lua
@@ -15,16 +15,23 @@ return {
           api_key = os.getenv "GITHUB_COPILOT",
         },
       },
-      chat_shortcut_respond = { modes = { "n", "i", "v", "x" }, shortcut = "<leader>cg" },
-      chat_shortcut_delete = { modes = { "n", "i", "v", "x" }, shortcut = "<leader>cd" },
+      user_input_ui = "buffer",
+      chat_shortcut_respond = { modes = { "n" }, shortcut = "<leader>cg" },
+      chat_shortcut_delete = { modes = { "n" }, shortcut = "<leader>cd" },
       chat_shortcut_stop = { modes = { "n", "i", "v", "x" }, shortcut = "<leader>cs" },
-      chat_shortcut_new = { modes = { "n", "i", "v", "x" }, shortcut = "<leader>cn" },
+      chat_shortcut_new = { modes = { "n" }, shortcut = "<leader>cn" },
       chat_free_cursor = true,
+      llm_prefix = "✨:",
+      chat_user_prefix = "👤:",
+      chat_confirm_delete = false,
     }
 
     -- keymaps
     vim.keymap.set({ "n", "x" }, "<leader>ca", "<cmd>PrtChatToggle<CR>", { noremap = true, silent = true })
     vim.keymap.set({ "v" }, "<leader>ca", ":<C-u>'<,'>PrtChatPaste<CR>", { noremap = true, silent = true })
     vim.keymap.set({ "v" }, "<leader>ce", "<cmd>PrtRewrite<CR>", { noremap = true, silent = true })
+    vim.keymap.set({ "n" }, "<leader>cf", "<cmd>PrtChatFinder<CR>", { noremap = true, silent = true })
+    vim.keymap.set({ "v" }, "<leader>ci", ":<C-u>'<,'>PrtImplement<CR>", { noremap = true, silent = true })
+    vim.keymap.set("v", "<leader>c?", ":<C-u>'<,'>PrtAsk<CR>", { noremap = true, silent = true })
   end
 }

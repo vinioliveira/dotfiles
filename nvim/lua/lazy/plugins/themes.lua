f8fe5d55c4920bbf78fc30498df12243c80a0198
Vinícius Oliveira <marcus@copy.ai>
2025-01-30 15:49:30 -0300

    More changes on alias and tool-versions

--- nvim/lua/lazy/plugins/themes.lua
+++ nvim/lua/lazy/plugins/themes.lua
@@ -36,24 +36,24 @@ return {
   --     transparent = true
   --   }
   -- },
-  -- {
-  --   "sainnhe/gruvbox-material",
-  --   lazy = false,
-  --   priority = 1000,
-  --   config = function()
-  --     -- Optionally configure and load the colorscheme
-  --     -- directly inside the plugin declaration.
-  --     vim.g.gruvbox_material_enable_italic = true
-  --     vim.g.gruvbox_material_transparent_background = 2
-  --     vim.g.gruvbox_material_enable_bold = true
+  {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.gruvbox_material_enable_italic = true
+      vim.g.gruvbox_material_transparent_background = 2
+      vim.g.gruvbox_material_enable_bold = true
 
-  --     vim.g.gruvbox_material_dim_inactive_windows = 1
-  --     -- vim.g.gruvbox_material_background = 'medium'
-  --     -- vim.g.gruvbox_material_statusline_style = 'default'
-  --     vim.g.gruvbox_material_foreground = 'material'
-  --     -- vim.g.gruvbox_material_better_performance = 1
-  --   end
-  -- },
+      vim.g.gruvbox_material_dim_inactive_windows = 1
+      -- vim.g.gruvbox_material_background = 'medium'
+      -- vim.g.gruvbox_material_statusline_style = 'default'
+      vim.g.gruvbox_material_foreground = 'material'
+      -- vim.g.gruvbox_material_better_performance = 1
+    end
+  },
   -- {
   --   'Tsuzat/NeoSolarized.nvim',
   --   opts = {

2b8eb53c24579ea7feb077f6102272408b63fe63
Vinícius Oliveira <marcus@copy.ai>
2025-03-22 11:41:59 -0300

    new fancy gruvbox black theme

--- nvim/lua/lazy/plugins/themes.lua
+++ nvim/lua/lazy/plugins/themes.lua
@@ -44,14 +44,24 @@ return {
       -- Optionally configure and load the colorscheme
       -- directly inside the plugin declaration.
       vim.g.gruvbox_material_enable_italic = true
-      vim.g.gruvbox_material_transparent_background = 2
+      -- vim.g.gruvbox_material_transparent_background = 2
       vim.g.gruvbox_material_enable_bold = true
 
+      vim.g.gruvbox_material_visual = "reverse"
+      vim.g.gruvbox_material_ui_contrast = "high"
+
       vim.g.gruvbox_material_dim_inactive_windows = 1
-      -- vim.g.gruvbox_material_background = 'medium'
+      vim.g.gruvbox_material_background = 'hard'
       -- vim.g.gruvbox_material_statusline_style = 'default'
       vim.g.gruvbox_material_foreground = 'material'
       -- vim.g.gruvbox_material_better_performance = 1
+      --
+      vim.g.gruvbox_material_float_style = 'dim'
+      --
+      vim.g.gruvbox_material_colors_override = {
+        ['bg0'] = { '#141617', '232' },
+        ['bg_dim'] = { '#1d2021', '234' },
+      }
     end
   },
   -- {
